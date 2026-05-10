35be88e790064a6a8429783c45a4da0a566288e2
Oleh Khmelevskoi <khmelevskoioleh@gmail.com>
2026-05-10 06:06:00 +0200

    create header

---
+++ app/components/AppHeader.vue
@@ -0,0 +1,22 @@
+<template>
+  <header class="header">
+    <div class="header__container">
+      <nav class="header__menu menu">
+        <!-- <div class="header__logo">Logo</div> -->
+
+        <ul class="header__list">
+          <li class="header__item">Menus</li>
+          <li class="header__item">About & Careers</li>
+          <li class="header__item">Contact</li>
+        </ul>
+      </nav>
+      <a href="#" class="header__logo"><span>debonaire</span></a>
+
+      <a class="header__book book"><span>Book an appointment</span></a>
+    </div>
+  </header>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
