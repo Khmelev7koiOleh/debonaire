0095685bc2d6931579eae36ee6a8293562a2dda4
Oleh Khmelevskoi <khmelevskoioleh@gmail.com>
2026-05-12 03:46:56 +0200

    add burger-menu

--- app/components/AppFooter.vue
+++ app/components/AppFooter.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>Footer</div>
+  <footer>Footer</footer>
 </template>
 
 <script setup lang="ts"></script>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,66 @@
 <template>
-  <div class="text">index page</div>
+  <div>
+    <div class="text">index page</div>
+    <div class="text">index page</div>
+    <div class="text">index page</div>
+    <div class="text">index page</div>
+    <div class="text">index page</div>
+    <div class="text">index page</div>
+    <div class="text">index page</div>
+    <div class="text">index page</div>
+    <div class="text">index page</div>
+    <div class="text">index page</div>
+    <div class="text">index page</div>
+    <div class="text">index page</div>
+    <div class="text">index page</div>
+    <div class="text">index page</div>
+    <div class="text">index page</div>
+    <div class="text">index page</div>
+    <div class="text">index page</div>
+    <div class="text">index page</div>
+    <div class="text">index page</div>
+    <div class="text">index page</div>
+    <div class="text">index page</div>
+    <div class="text">index page</div>
+    <div class="text">index page</div>
+    <div class="text">index page</div>
+    <div class="text">index page</div>
+    <div class="text">index page</div>
+    <div class="text">index page</div>
+    <div class="text">index page</div>
+    <div class="text">index page</div>
+    <div class="text">index page</div>
+    <div class="text">index page</div>
+    <div class="text">index page</div>
+    <div class="text">index page</div>
+    <div class="text">index page</div>
+    <div class="text">index page</div>
+    <div class="text">index page</div>
+    <div class="text">index page</div>
+    <div class="text">index page</div>
+    <div class="text">index page</div>
+    <div class="text">index page</div>
+    <div class="text">index page</div>
+    <div class="text">index page</div>
+    <div class="text">index page</div>
+    <div class="text">index page</div>
+    <div class="text">index page</div>
+    <div class="text">index page</div>
+    <div class="text">index page</div>
+    <div class="text">index page</div>
+    <div class="text">index page</div>
+    <div class="text">index page</div>
+    <div class="text">index page</div>
+    <div class="text">index page</div>
+    <div class="text">index page</div>
+    <div class="text">index page</div>
+    <div class="text">index page</div>
+    <div class="text">index page</div>
+    <div class="text">index page</div>
+    <div class="text">index page</div>
+    <div class="text">index page</div>
+    <div class="text">index page</div>
+  </div>
 </template>
 
 <script setup lang="ts"></script>
