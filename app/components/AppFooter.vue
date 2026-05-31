fd022853af63962fc6d6f5b5aa7d109a7520b8fc
Oleh Khmelevskoi <khmelevskoioleh@gmail.com>
2026-05-31 09:11:27 +0200

    add footed

--- app/components/AppFooter.vue
+++ app/components/AppFooter.vue
@@ -1,5 +1,32 @@
 <template>
-  <footer>Footer</footer>
+  <footer class="footer">
+    <div class="footer__container">
+      <ul class="footer__menu menu-footer">
+        <li>
+          <a href="#" class="menu-footer__link text">Terms of Service</a>
+        </li>
+
+        <li>
+          <a href="#" class="menu-footer__link text">Instagram</a>
+        </li>
+        <li>
+          <a href="#" class="menu-footer__link text">Privacy Policy</a>
+        </li>
+        <li>
+          <a href="#" class="menu-footer__link text"> Facebook</a>
+        </li>
+      </ul>
+
+      <div class="footer__important important">
+        <div class="important__container">
+          <div class="important__title title">
+            We invite you to join our team.
+          </div>
+          <a href="#" class="important__link title"><span>Apply Now</span></a>
+        </div>
+      </div>
+    </div>
+  </footer>
 </template>
 
 <script setup lang="ts"></script>
